4bf6a16205c0be67f19b16fd066d5ddd07f1ca02
陳庭浩 <112939670+Howard3897613@users.noreply.github.com>
2025-03-15 14:04:19 +0800

    firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,4 +59,10 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+
+    // Add the dependency for the Realtime Database library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-database")
 }
